c8c3dfc1155e5bdfc0544fb3101ea4e84a98a39b
sh06-25 <155663427+sh06-25@users.noreply.github.com>
2024-06-20 14:37:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,143 @@
+let fPage = document.querySelector(".fPage");
+let sPage = document.querySelector(".sPage");
+let thPage = document.querySelector(".thPage");
+let foPage = document.querySelector(".foPage");
+let fiPage = document.querySelector(".fiPage");
+let siPage = document.querySelector(".siPage");
+let sePage = document.querySelector(".sePage");
+let ePage = document.querySelector(".ePage");
+let nPage = document.querySelector(".nPage");
+
+const A = document.getElementById("A");
+const AB = document.getElementById("AB");
+const AC = document.getElementById("AC");
+const AD = document.getElementById("AD");
+
+const B = document.getElementById("B");
+const BB = document.getElementById("BB");
+const BC = document.getElementById("BC");
+
+const C = document.getElementById("C");
+const CB = document.getElementById("CB");
+const CC = document.getElementById("CC");
+
+const D = document.getElementById("D");
+const DB = document.getElementById("DB");
+const DC = document.getElementById("DC");
+const DD = document.getElementById("DD");
+const ED = document.getElementById("ED");
+
+
+A.addEventListener('click', function(){
+    fPage.style.display = "none";
+    sPage.style.display = "block";
+  
+  })
+
+  AB.addEventListener('click', function(){
+    fPage.style.display = "none";
+    sPage.style.display = "block";
+  
+  })
+
+  AC.addEventListener('click', function(){
+    fPage.style.display = "none";
+    sPage.style.display = "block";
+  
+  })
+
+  AD.addEventListener('click', function(){
+    fPage.style.display = "none";
+    sPage.style.display = "block";
+  
+  })
+  
+  B.addEventListener('click', function(){
+    fPage.style.display = "none";
+    sPage.style.display = "none";
+    thPage.style.display = "block";
+  
+  })
+
+  BB.addEventListener('click', function(){
+    fPage.style.display = "none";
+    sPage.style.display = "none";
+    thPage.style.display = "block";
+  
+  })
+
+  BC.addEventListener('click', function(){
+    fPage.style.display = "none";
+    sPage.style.display = "none";
+    thPage.style.display = "block";
+  })
+
+  C.addEventListener('click', function(){
+    fPage.style.display = "none";
+    sPage.style.display = "none";
+    thPage.style.display = "none";
+    foPage.style.display = "block";
+  
+  })
+
+  CB.addEventListener('click', function(){
+    fPage.style.display = "none";
+    sPage.style.display = "none";
+    thPage.style.display = "none";
+    foPage.style.display = "block";
+  
+  })
+
+  CC.addEventListener('click', function(){
+    fPage.style.display = "none";
+    sPage.style.display = "none";
+    thPage.style.display = "none";
+    foPage.style.display = "block";
+  
+  })
+
+  D.addEventListener('click', function(){
+    fPage.style.display = "none";
+    sPage.style.display = "none";
+    thPage.style.display = "none";
+    foPage.style.display = "none";
+    fiPage.style.display = "block";
+  
+  })
+
+  DB.addEventListener('click', function(){
+    fPage.style.display = "none";
+    sPage.style.display = "none";
+    thPage.style.display = "none";
+    foPage.style.display = "none";
+    siPage.style.display = "block";
+  
+  })
+
+  DC.addEventListener('click', function(){
+    fPage.style.display = "none";
+    sPage.style.display = "none";
+    thPage.style.display = "none";
+    foPage.style.display = "none";
+    sePage.style.display = "block";
+  
+  })
+
+  DD.addEventListener('click', function(){
+    fPage.style.display = "none";
+    sPage.style.display = "none";
+    thPage.style.display = "none";
+    foPage.style.display = "none";
+    ePage.style.display = "block";
+  
+  })
+
+  ED.addEventListener('click', function(){
+    fPage.style.display = "none";
+    sPage.style.display = "none";
+    thPage.style.display = "none";
+    foPage.style.display = "none";
+    ePage.style.display = "none";
+    nPage.style.display = "block";
+  
+  })
